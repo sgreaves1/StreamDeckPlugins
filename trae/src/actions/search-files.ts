519305a581251710a48f8f23d2d917149d4670fe
sam greaves <samgreaves0@gmail.com>
2025-07-08 15:12:07 +0100

    Add search files button to trae

---
+++ trae/src/actions/search-files.ts
@@ -0,0 +1,26 @@
+import { action, KeyDownEvent, SingletonAction } from "@elgato/streamdeck";
+import { exec } from "child_process";
+import { promisify } from "util";
+
+const execAsync = promisify(exec);
+
+@action({UUID: "com.sam-greaves.trae.search-files"})
+export class SearchFiles extends SingletonAction {
+    override async onKeyDown(ev: KeyDownEvent): Promise<void> {
+        try {
+            console.log("Plugin initialized");
+
+            // AppleScript to simulate go to definition
+            const script = `
+                tell application "System Events"
+                    tell process "Trae"
+                        click menu item "Go to File…" of menu "Go" of menu bar 1
+                    end tell
+                end tell
+            `;
+            await execAsync(`osascript -e '${script}'`);
+        } catch (error) {
+            console.error('Failed to simulate "Go to Definition": ', error);
+        }
+    }
+}
--- trae/src/plugin.ts
+++ trae/src/plugin.ts
@@ -1,7 +1,8 @@
 import streamDeck, { LogLevel } from "@elgato/streamdeck";
 
-import { LaunchTrae } from "./actions/launch-trae";
+import { LaunchTrae } from "./actions/launch";
 import { GoToDefinition } from "./actions/go-to-definition";
+import { SearchFiles } from "./actions/search-files";
 
 // We can enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information
 streamDeck.logger.setLevel(LogLevel.TRACE);
@@ -9,6 +10,7 @@ streamDeck.logger.setLevel(LogLevel.TRACE);
 // Register the actions.
 streamDeck.actions.registerAction(new LaunchTrae());
 streamDeck.actions.registerAction(new GoToDefinition());
+streamDeck.actions.registerAction(new SearchFiles());
 
 // Finally, connect to the Stream Deck.
 streamDeck.connect();
